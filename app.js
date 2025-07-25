bf08d04c31769c8210152933bda9f8dc647aa650
jplasencia-cwm <jplasencia@cwmatthews.com>
2025-07-25 16:16:03 -0400

    created autoupdate

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js')
-    .then(() => console.log('Service Worker Registered'));
+  navigator.serviceWorker.register('service-worker.js').then(reg => {
+    reg.onupdatefound = () => {
+      const newWorker = reg.installing;
+      newWorker.onstatechange = () => {
+        if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
+          alert('New version available! Refresh to update.');
+        }
+      };
+    };
+  });
 }
--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,26 @@
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = ['/', '/index.html', '/app.js', '/manifest.json'];
+
 self.addEventListener('install', event => {
-  console.log('Service Worker installing.');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) return caches.delete(name);
+        })
+      )
+    )
+  );
+});
+
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
